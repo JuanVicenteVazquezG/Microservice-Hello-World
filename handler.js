b15c829345506234384c2395719ab1c0f31b629c
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-06-02 11:40:53 +0200

    Peticiones post

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const queryString = require('querystring');
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
@@ -8,11 +10,20 @@ module.exports.hello = async (event, context) => {
         message: `Hola ${event.pathParameters.name}`,
         input: event.name,
       },
-      null,
-      2
     ),
-  };
+  }
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+module.exports.showUser = async (event, context) => {
+  const { body } = queryString.parse(event['body']);
+  console.log(body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Petición POST`,
+        input: `Hola ${body.name} ${body.lastname}`,
+      })
+  }
 };
