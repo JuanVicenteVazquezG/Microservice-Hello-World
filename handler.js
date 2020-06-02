68fd5fd3a7a8e80c51d64d6529ecdc52e2f79331
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-06-02 12:22:10 +0200

    first deploy to AWS

--- handler.js
+++ handler.js
@@ -1,29 +1,24 @@
 'use strict';
-
 const queryString = require('querystring');
 
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Hola ${event.pathParameters.name}`,
-        input: event.name,
-      },
-    ),
-  }
+    body: JSON.stringify({
+      message: `Hola ${event.pathParameters.name}`,
+      input: event.name,
+    }),
+  };
 };
 
 
 module.exports.showUser = async (event, context) => {
-  const { body } = queryString.parse(event['body']);
-  console.log(body);
+  const body = queryString.parse(event['body']);
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Petición POST`,
-        input: `Hola ${body.name} ${body.lastname}`,
-      })
-  }
+    body: JSON.stringify({
+      message: `Petición POST`,
+      input: `Hola ${body.name} ${body.lastname}`,
+    }),
+  };
 };
